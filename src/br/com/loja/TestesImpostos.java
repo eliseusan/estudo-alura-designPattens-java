c098946a9ce823c19f56f598ea106a9b43db355f
Eliseu <eliseusantico@gmail.com>
2022-11-20 12:19:51 -0300

    Resolucao de problema com Strategy

--- src/br/com/loja/TestesImpostos.java
+++ src/br/com/loja/TestesImpostos.java
@@ -3,17 +3,14 @@ package br.com.loja;
 import java.math.BigDecimal;
 
 import br.com.loja.imposto.CalculadoraDeImpostos;
-import br.com.loja.imposto.TipoImposto;
+import br.com.loja.imposto.ISS;
 import br.com.loja.orcamento.Orcamento;
 
 public class TestesImpostos {
 
 	public static void main(String[] args) {
-	
 		Orcamento orcamento = new Orcamento(new BigDecimal("100"));
 		CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
-		
-		System.out.println(calculadora.calcular(orcamento,TipoImposto.ISS));
+		System.out.println(calculadora.calcular(orcamento,new ISS()));
 	}
-
 }
